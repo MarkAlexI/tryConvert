e0e3843c01468ce44c3fc9dfaa7194dfb1482313
MarkAlexI <85037509+MarkAlexI@users.noreply.github.com>
2022-06-12 19:37:43 +0300

    Update run.js - add function lengthConvert.

--- src/run.js
+++ src/run.js
@@ -29,3 +29,37 @@ function numberConvert() {
     if (isNaN(result) || !checkInput) result = "введенное число не валидно";
     output.innerHTML = "Результат: " + result;
   }
+  
+function lengthConvert() {
+  let output = document.getElementById("out2");
+  let input = this.window.document.getElementById("input2").value;
+
+  let from = this.window.document.getElementById("from2").value;
+  let to = this.window.document.getElementById("second_dropdown").value;
+  let koef = 10;
+  let result = 0;
+  
+  if (from === "м" && to === "м") koef = 1;
+  if (from === "м" && to === "фут") koef = 0.31;
+  if (from === "м" && to === "миля") koef = 1/1680;
+  if (from === "м" && to === "км") koef = 0.001;
+  
+  if (from === "фут" && to === "м") koef = 2.9;
+  if (from === "фут" && to === "фут") koef = 1;
+  if (from === "фут" && to === "миля") koef = 0.000189;
+  if (from === "фут" && to === "км") koef = 2900;
+  
+  if (from === "миля" && to === "м") koef = 1680;
+  if (from === "миля" && to === "фут") koef = 1/0.000189;
+  if (from === "миля" && to === "миля") koef = 1;
+  if (from === "миля" && to === "км") koef = 0.7;
+  
+  if (from === "км" && to === "м") koef = 0.001;
+  if (from === "км" && to === "фут") koef = 0.001/2.9;
+  if (from === "км" && to === "миля") koef = 1/1.68;
+  if (from === "км" && to === "км") koef = 1;
+  
+  result = input * koef;
+  if (input < 0 || isNaN(result)) result = "введенное число не валидно";
+  output.innerHTML = "Результат: " + result;
+}
